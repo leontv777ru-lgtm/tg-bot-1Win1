e8577c35cb1f668f54bad9455c296c6007ce610f
leontv777ru-lgtm <leontv777.ru@mail.ru>
2026-03-05 17:56:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 
 from aiogram import Dispatcher, Bot
+from aiogram.types import Message
 
 from config import BOT_TOKEN
 from handlers.client import router as client_router
@@ -15,26 +16,32 @@ logger = logging.getLogger(__name__)
 async def main():
     logging.basicConfig(
         level=logging.INFO,
-        format=f'[BOT] '
-               f'{u"%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s"}')
+        format='[BOT] %(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s'
+    )
+
     logger.info("Starting bot...")
 
     dp = Dispatcher()
     bot = Bot(BOT_TOKEN)
+
     dp.include_routers(client_router, admin_router)
 
-POSTBACK_CHANNEL = -1003712007488
+    POSTBACK_CHANNEL = -1003712007488
 
-@dp.channel_post()
-async def postback_handler(message):
-    if message.chat.id == POSTBACK_CHANNEL:
-        try:
-            user_id = int(message.text)
-            await DataBase.update_verifed(user_id)
-        except:
-            pass
+    @dp.channel_post()
+    async def postback_handler(message: Message):
+        if message.chat.id == POSTBACK_CHANNEL:
+            try:
+                user_id = int(message.text)
+                await DataBase.update_verifed(user_id)
+            except:
+                pass
 
-dp.startup.register(DataBase.on_startup)
-await bot.delete_webhook(drop_pending_updates=True)
+    dp.startup.register(DataBase.on_startup)
 
-await dp.start_polling(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
